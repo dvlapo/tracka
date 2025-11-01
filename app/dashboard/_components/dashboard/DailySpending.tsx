feda95a7fe5e88298064b5462a2390465acd9b65
dvlapo <agbooladolapo24@gmail.com>
2025-11-01 20:42:32 +0100

    add charts

--- app/dashboard/_components/dashboard/DailySpending.tsx
+++ app/dashboard/_components/dashboard/DailySpending.tsx
@@ -1,7 +1,52 @@
+import {formatCurrency} from '@/app/utils';
+import {
+  Tooltip,
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+} from 'recharts';
+
+const dailySpending = [
+  {date: '2025-10-20', total: 400},
+  {date: '2025-10-21', total: 200},
+  {date: '2025-10-22', total: 10},
+  {date: '2025-10-23', total: 0},
+  {date: '2025-10-24', total: 900},
+  {date: '2025-10-25', total: 1300},
+  {date: '2025-10-26', total: 1000},
+];
+
 export default function DailySpending() {
   return (
     <div className="squircle border border-gray-300 rounded-2xl p-4">
-      <h3 className="text-sm">Daily Spending (last 7 days)</h3>
+      <h3 className="text-sm mb-6">Daily Spending (last 7 days)</h3>
+
+      <ResponsiveContainer width="100%" height={250}>
+        <LineChart data={dailySpending} className="text-sm">
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Tooltip
+            // formatter={(value, name) => {
+            //   return [formatCurrency(value as number), name];
+            // }}
+            labelClassName="text-sm text-foreground"
+            contentStyle={{
+              fontSize: '0.875rem',
+            }}
+          />
+          <Line
+            type="monotone"
+            dataKey="total"
+            stroke="currentColor"
+            strokeWidth={2}
+            dot={{r: 4}}
+          />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 }
--- app/dashboard/_components/dashboard/SpendingByCategory.tsx
+++ app/dashboard/_components/dashboard/SpendingByCategory.tsx
@@ -1,7 +1,51 @@
+import {formatCurrency} from '@/app/utils';
+import {PieChart, Pie, Cell, Tooltip, ResponsiveContainer} from 'recharts';
+
+const COLORS = ['#4A90E2', '#50E3C2', '#F5A623', '#D0021B'];
+
+const spendingByCategory = [
+  {category: 'Transportation', total: 600, percentage: 60},
+  {category: 'Groceries', total: 250, percentage: 25},
+  {category: 'Entertainment', total: 150, percentage: 15},
+];
+
 export default function SpendingByCategory() {
   return (
     <div className="squircle border border-gray-300 rounded-2xl p-4">
-      <h3 className="text-sm">Spending by Category</h3>
+      <h3 className="text-sm mb-6">Spending by Category</h3>
+
+      <ResponsiveContainer width="100%" height={250}>
+        <PieChart>
+          <Pie
+            data={spendingByCategory}
+            dataKey="total"
+            nameKey="category"
+            cx="50%"
+            cy="50%"
+            outerRadius={80}
+            label={({name, percent}: any) =>
+              `${name} ${(percent * 100).toFixed(1)}%`
+            }
+            className="text-xs"
+          >
+            {spendingByCategory?.map((_, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+          <Tooltip
+            formatter={(value, name) => {
+              return [formatCurrency(value as number), name];
+            }}
+            labelClassName="text-sm"
+            contentStyle={{
+              fontSize: '0.875rem',
+            }}
+          />
+        </PieChart>
+      </ResponsiveContainer>
     </div>
   );
 }
